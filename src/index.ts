24cf2ecb001828d3d714795c5e9938b1c1b2ee2d
Ismailspons12 <Ismail.jellali.spons@gmail.com>
2026-02-10 11:32:08 +0100

    Add medicaments route to the application

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import cors from 'cors'
 import { route as barcodeRoute } from './routes/barcode'
 import { route as priceRoute } from './routes/price'
 import { route as providerRoute } from './routes/provider'
-import { route as medicamentsRoute } from './routes/medicaments' // AJOUTE CETTE LIGNE
+import { route as medicamentsRoute } from './routes/medicaments'
 
-const routes = [barcodeRoute, priceRoute, providerRoute, medicamentsRoute] // AJOUTE medicamentsRoute ICI
+const routes = [barcodeRoute, priceRoute, providerRoute, medicamentsRoute]
+
+export const app = express()
+
+app.use(cors({ origin: '*' }))
+
+routes.forEach(route => app.use('/api', route))
